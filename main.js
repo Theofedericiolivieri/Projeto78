7df114ed4cc4955d73e951fc60e8a5354102e390
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2022-09-05 17:17:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var fotos=["irma.jpg","pai-e-filho-png.png","mae.png"];
 var nomes=["lia","Jack","Mary"];
 var count=0;
 function atualizar(){
-    i++;
+    count++;
     if(count>2){
         count=0;
     }
     var atualizarfoto=fotos[count];
     var atualizarnome=nomes[count];
-    document.getElementById(fotos).src=atualizarfoto;
-    document.getElementById(nomes).src=atualizarnome;
+    document.getElementById("Foto").src=atualizarfoto;
+    document.getElementById("nome").innerHTML=atualizarnome;
 }
